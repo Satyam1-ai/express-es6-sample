a7f852187958bb0d425e4986b220b53cd2fc57e1
satyam1-ai <satyamdash0000@gmail.com>
2024-10-30 03:11:37 +0800

    Keep port 3000 for Elastic Beanstalk

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const router = express.Router();  // This initializes the router
+
+// Define the route
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Student ID is 21756630. Welcome to Express' });
 });
+
+module.exports = router;  // Export the router for use in other files
 
