eadfa5f9673ae6df72512a7c1004904f2f89998b
satyam1-ai <satyamdash0000@gmail.com>
2024-10-30 00:42:32 +0800

    Update welcome message with student ID

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,4 @@
-import express from 'express';
-
-var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.render('index', { title: 'My Student ID is 21756630. Welcome to Express' });
 });
 
-export default router;
